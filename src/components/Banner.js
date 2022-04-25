43f3c0c0bbc6bd6804ba5c49c3f9d2f8096965e1
Victor Elias <svvictorelias@gmail.com>
2022-04-24 21:46:48 -0300

    adicionado descrição com limite de 150 letras

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -23,6 +23,10 @@ function Banner() {
         fetchRandomMovie();
     }, []);
     
+    function truncate(str, n){
+        return str?.length > n ? str.substring(0, n - 1) + "..." : str;
+    }
+
   return (
     <header
     className='banner-container'
@@ -31,7 +35,24 @@ function Banner() {
         backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
         roundPosition: 'center-center',
     }}
-    ></header>);
+    >
+        <div className='banner-content'>
+            <h1 className='banner-title'>
+                {movie?.title || movie?.name || movie?.original_name}
+            </h1>
+            <div className='banner-button-container'>
+                <button className='banner-button'>
+                    Assistir
+                </button>
+                <button className='banner-button'>
+                    Minha Lista
+                </button>
+            </div>
+            <div className='banner-description'>
+                <h2>{truncate(movie?.overview,150)}</h2>
+            </div>
+        </div>
+    </header>);
 }
 
 
